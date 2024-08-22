2985fe29265ee52da0c5431fa3da256b454f4281
BeomSu Kim <33516980+Indigochi1d@users.noreply.github.com>
2024-08-22 22:16:58 +0900

    [2단계] 김범수 미션 제출합니다!

--- src/Containers/Main.jsx
+++ src/Containers/Main.jsx
@@ -1,11 +1,33 @@
 import Category from "../components/Category.jsx";
 import RestaurantList from "../components/RestaurantList.jsx";
+import {useState} from "react";
+import {restaurantsInfo} from "../data/datas.js";
 
 const Main = () => {
+    const [category,setCategory] = useState("전체");
+    const onCategoryChange = (e) => {
+        setCategory(e.target.value);
+    }
+    /*방법1*/
+    let filteredRestaurants;
+    if(category === "전체"){
+        filteredRestaurants = restaurantsInfo;
+    }
+    else{
+        filteredRestaurants = restaurantsInfo.filter(restaurant =>
+            restaurant.category === category
+        )
+    }
+    /*방법2 - 삼항연산자 사용*/
+    /*
+        filteredRestaurants= category === "전체" ?
+        restaurantsInfo:
+        restaurantsInfo.filter(restaurant => restaurant.category === category);
+     */
     return (
         <>
-            <Category/>
-            <RestaurantList/>
+            <Category category={category} onCategoryChange={onCategoryChange}/>
+            <RestaurantList restaurants={filteredRestaurants}/>
         </>
     );
 };
--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,10 +1,16 @@
 import styles from "../css/Category.module.css";
 
-const Category = () => {
+const Category = ({category,onCategoryChange}) => {
     const options = ["전체","한식", "중식", "일식", "양식", "아시안", "기타"];
     return (
         <section className={styles[`restaurant-filter-container`]}>
-            <select name="category" id="category-filter" className="restaurant-filter" aria-label="음식점 카테고리 필터">
+            <select name="category"
+                    id="category-filter"
+                    className="restaurant-filter"
+                    aria-label="음식점 카테고리 필터"
+                    value={category}
+                    onChange={onCategoryChange}
+            >
                 {options.map((option, index) => (
                     <option key={index} value={option}>
                         {option}
