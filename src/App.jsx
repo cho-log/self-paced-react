db3effd7319bfd85df6c3ab74c0cef32f1a92400
songhj10207@naver.com <songhj10207@naver.com>
2024-10-08 15:44:39 +0900

    feat: rendering-lists 기능 구현

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,27 @@ import CategoryFilter from './components/main/categoryFilter/CategoryFilter';
 import RestaurantList from './components/main/restaurant/RestaurantList';
 import AddRestaurantModal from './components/aside/AddRestaurantModal';
 import RestaurantDetailModal from './components/aside/RestaurantDetailModal';
+import { useState } from 'react';
+import { restaurants } from './components/constants/Restaurants';
+
 function App() {
+  const [category, setCategory] = useState('전체');
+  let filteredRestaurants = [];
+  if (category === '전체') {
+    filteredRestaurants = restaurants;
+  } else {
+    restaurants.map((item) => {
+      if (item.category === category) {
+        filteredRestaurants.push(item);
+      }
+    });
+  }
   return (
     <>
       <Header />
       <main>
-        <CategoryFilter />
-        <RestaurantList />
+        <CategoryFilter category={category} onChangeCategory={setCategory} />
+        <RestaurantList restaurants={filteredRestaurants} />
       </main>
       <aside>
         <RestaurantDetailModal />
--- src/components/main/restaurant/RestaurantCard.jsx
+++ src/components/main/restaurant/RestaurantCard.jsx
@@ -1,25 +1,42 @@
 import styles from '../../../css/Restaurant.module.css';
-const RestaurantCard = ({
-  category_image,
-  image_alt,
-  restaurant_name,
-  restaurant_description,
-}) => {
+import KoreanIcon from '../../../assets/category/category-korean.png';
+import AsianIcon from '../../../assets/category/category-asian.png';
+import ChineseIcon from '../../../assets/category/category-chinese.png';
+import EtcIcon from '../../../assets/category/category-etc.png';
+import JapaneseIcon from '../../../assets/category/category-japanese.png';
+import WesternIcon from '../../../assets/category/category-western.png';
+
+const RestaurantCard = ({ alt, name, description }) => {
+  let image;
+  switch (alt) {
+    case '한식':
+      image = KoreanIcon;
+      break;
+    case '중식':
+      image = ChineseIcon;
+      break;
+    case '아시안':
+      image = AsianIcon;
+      break;
+    case '일식':
+      image = JapaneseIcon;
+      break;
+    case '양식':
+      image = WesternIcon;
+      break;
+    case '기타':
+      image = EtcIcon;
+      break;
+  }
   return (
     <li className={styles.restaurant}>
       <div className={styles.restaurant__category}>
-        <img
-          src={category_image}
-          alt={image_alt}
-          className={styles.categoryIcon}
-        />
+        <img src={image} alt={alt} className={styles.categoryIcon} />
       </div>
       <div className={styles.restaurant__info}>
-        <h3 className={`${styles.restaurant__name} text-subtitle`}>
-          {restaurant_name}
-        </h3>
+        <h3 className={`${styles.restaurant__name} text-subtitle`}>{name}</h3>
         <p className={`${styles.restaurant__description}  text-body`}>
-          {restaurant_description}
+          {description}
         </p>
       </div>
     </li>
