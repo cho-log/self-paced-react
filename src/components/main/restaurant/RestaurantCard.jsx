f429b0f4ca0660ecd7914fa285c6f3f4b670208b
hae_oo <songhj10207@naver.com>
2024-10-08 15:51:14 +0900

    Revert "feat: rendering-lists 기능 구현"

--- src/components/main/restaurant/RestaurantCard.jsx
+++ src/components/main/restaurant/RestaurantCard.jsx
@@ -1,42 +1,25 @@
 import styles from '../../../css/Restaurant.module.css';
-import KoreanIcon from '../../../assets/category/category-korean.png';
-import AsianIcon from '../../../assets/category/category-asian.png';
-import ChineseIcon from '../../../assets/category/category-chinese.png';
-import EtcIcon from '../../../assets/category/category-etc.png';
-import JapaneseIcon from '../../../assets/category/category-japanese.png';
-import WesternIcon from '../../../assets/category/category-western.png';
-
-const RestaurantCard = ({ alt, name, description }) => {
-  let image;
-  switch (alt) {
-    case '한식':
-      image = KoreanIcon;
-      break;
-    case '중식':
-      image = ChineseIcon;
-      break;
-    case '아시안':
-      image = AsianIcon;
-      break;
-    case '일식':
-      image = JapaneseIcon;
-      break;
-    case '양식':
-      image = WesternIcon;
-      break;
-    case '기타':
-      image = EtcIcon;
-      break;
-  }
+const RestaurantCard = ({
+  category_image,
+  image_alt,
+  restaurant_name,
+  restaurant_description,
+}) => {
   return (
     <li className={styles.restaurant}>
       <div className={styles.restaurant__category}>
-        <img src={image} alt={alt} className={styles.categoryIcon} />
+        <img
+          src={category_image}
+          alt={image_alt}
+          className={styles.categoryIcon}
+        />
       </div>
       <div className={styles.restaurant__info}>
-        <h3 className={`${styles.restaurant__name} text-subtitle`}>{name}</h3>
+        <h3 className={`${styles.restaurant__name} text-subtitle`}>
+          {restaurant_name}
+        </h3>
         <p className={`${styles.restaurant__description}  text-body`}>
-          {description}
+          {restaurant_description}
         </p>
       </div>
     </li>
