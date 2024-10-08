6c6c45b4cba9a3cba9ea742d0e1ac75a6063b689
Yeeun Park <ye6194@naver.com>
2024-10-08 15:07:48 +0900

    feat: 필터된 음식점 목록을 보여줄 때 카테고리에 맞는 이미지 출력

--- src/components/RestaurantList.jsx
+++ src/components/RestaurantList.jsx
@@ -1,6 +1,15 @@
 import "../styles/RestaurantListStyle.css";
 
 function RestaurantList({ restaurants }) {
+  const categoryInEnglish = {
+    한식: "korean",
+    중식: "chinese",
+    일식: "japanese",
+    양식: "western",
+    아시안: "asian",
+    기타: "etc",
+  };
+
   return (
     <>
       <section className="restaurant-list-container">
@@ -8,7 +17,11 @@ function RestaurantList({ restaurants }) {
           {restaurants.map((restaurant) => (
             <li key={restaurant.id} className="restaurant">
               <div className="restaurant__category">
-                <img src="/category-korean.png" alt="한식" className="category-icon" />
+                <img
+                  src={`/category-${categoryInEnglish[restaurant.category]}.png`}
+                  alt="한식"
+                  className="category-icon"
+                />
               </div>
               <div className="restaurant__info">
                 <h3 className="restaurant__name text-subtitle">{restaurant.name}</h3>
