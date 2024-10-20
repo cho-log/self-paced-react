5101e368ed3e82029c9d942e3577971baa9e102a
bae-kh <qorkdgus010@gmail.com>
2024-10-20 20:45:55 +0900

    refeat: datas 생성 후 코드 짧게 수정

--- src/mainbody/CategoryFilter.jsx
+++ src/mainbody/CategoryFilter.jsx
@@ -1,5 +1,5 @@
 import styles from "../css/CategoryFilter.module.css";  
-
+import { options } from "../datas/RestaurantData";
 function CategoryFilter() {
   return (
     <section className={styles["restaurant-filter-container"]}>
@@ -9,13 +9,11 @@ function CategoryFilter() {
         className={styles["restaurant-filter"]}
         aria-label="음식점 카테고리 필터"
       >
-        <option value="전체">전체</option>
-        <option value="한식">한식</option>
-        <option value="중식">중식</option>
-        <option value="일식">일식</option>
-        <option value="양식">양식</option>
-        <option value="아시안">아시안</option>
-        <option value="기타">기타</option>
+        {options.map((option, index) => (
+          <option key={index} value={option}>
+            {option}
+          </option>
+        ))}
       </select>
     </section>
   );
