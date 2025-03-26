eb4f2dcd8717a309e59383151eff53d3d1b7eeaf
gxuoo <rbdud189@naver.com>
2025-03-27 01:34:53 +0900

    feat: Aside 컴포넌트 구현
    레스토랑의 정보를 띄워주는 컴포넌트, 레스토랑을 추가할 수 있는 컴포넌트를 구현했습니다.

---
+++ src/components/aside/RestaurantModal.jsx
@@ -0,0 +1,11 @@
+import RestaurantInformationModal from "./RestaurantInformationModal.jsx";
+import RestaurantAddModal from "./RestaurantAddModal.jsx";
+
+export default function RestaurantModal() {
+    return (
+        <>
+            <RestaurantInformationModal />
+            <RestaurantAddModal />
+        </>
+    );
+}
