e373469ddea13bfbc37e3233cbb39a3078489229
gxuoo <rbdud189@naver.com>
2025-03-27 01:31:00 +0900

    feat: Header 컴포넌트 구현
    HeadContainer에 들어가는 Header 컴포넌트 구현

--- src/components/head/HeadContainer.jsx
+++ src/components/head/HeadContainer.jsx
@@ -1,7 +1,12 @@
 import "/src/styles/Header.css"
+import Header from "./Header.jsx"
+
 export default function HeadContainer() {
     return (
         <>
+            <header className="gnb">
+                <Header />
+            </header>
         </>
     );
 }
