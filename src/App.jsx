c9a6a1b8adc001cffdaf526fa73df2046ad46c8a
ChangwooJ <wjdckddn99@naver.com>
2025-03-26 21:14:25 +0900

    Refactor: 기능별로 컴포넌트화 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import "./App.css";
+import Aside from "./pages/Aside";
+import Body from "./pages/Body";
+import Header from "./pages/Header";
 
 function App() {
-  return <h1>Self-Paced React</h1>;
+  return(
+    <>
+      <Header></Header>
+      <Body></Body>
+      <Aside></Aside>
+    </>
+  )
 }
 
 export default App;
---
+++ src/pages/Body.jsx
@@ -0,0 +1,13 @@
+import CategorySortFilter from "../component/body/CategorySortFilter";
+import RestaurantList from "../component/body/RestaurantList";
+
+const Body = () => {
+    return (
+        <>
+            <CategorySortFilter></CategorySortFilter>
+            <RestaurantList></RestaurantList>
+        </>
+    )
+}
+
+export default Body;
