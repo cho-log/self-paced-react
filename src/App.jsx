489d1cdb7fca273f28547537cbf128b3b0fe069d
INSANE-P <chanbin0626@gmail.com>
2025-03-27 20:20:56 +0900

    feat: component 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import "./App.css";
+import Header from "./components/header/Header";
+import CategoryFilter from "./components/main/CategoryFilter";
+import RestaurantList from "./components/main/RestaurantList";
+import RestaurantDetailModal from "./components/aside/RestaurantDetailModal";
+import AddRestaurantModal from "./components/aside/AddRestaurantModal";
 
 function App() {
-  return <h1>Self-Paced React</h1>;
+  return (
+    <>
+      <Header />
+      <main>
+        <CategoryFilter />
+        <RestaurantList />
+      </main>
+      <aside>
+        <RestaurantDetailModal />
+        <AddRestaurantModal />
+      </aside>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/main/CategoryFilter.jsx
@@ -0,0 +1,21 @@
+import styles from "./css/CategoryFilter.module.css";
+import categories from "../../data/categories";
+
+export default function CategoryFilter() {
+    return (
+        <section className={styles["restaurant-filter-container"]}>
+        <select 
+        name="category" 
+        id="category-filter" 
+        className={styles["restaurant-filter"]} 
+        aria-label="음식점 카테고리 필터"
+        >
+            {categories.map((category) => (
+                <option key={category} value={category}>
+                    {category}
+                </option>
+            ))}
+        </select>
+    </section>
+    );
+}
