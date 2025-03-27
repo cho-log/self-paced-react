d153b375368f438d2a449d163de51ba8c985c712
Shin Jihoon <tobeabestns@naver.com>
2025-03-28 01:17:07 +0900

    feat: Restaurant 정보/추가(aside) 관련 css 모듈 생성

--- src/components/asides/AddRestaurantModal.jsx
+++ src/components/asides/AddRestaurantModal.jsx
@@ -1,11 +1,13 @@
+import styles from "./RestaurantModal.model.css";
+
 function AddRestaurantModal() {
-    return (
-    <div className="modal modal--open">
-      <div className="modal-backdrop"></div>
-      <div className="modal-container">
-        <h2 className="modal-title text-title">새로운 음식점</h2>
+  return (
+    <div className={styles["modal modal--open"]}>
+      <div className={styles["modal-backdrop"]}></div>
+      <div className={styles["modal-container"]}>
+        <h2 className={styles["modal-title text-title"]}>새로운 음식점</h2>
         <form>
-          <div className="form-item form-item--required">
+          <div className={styles["form-item form-item--required"]}>
             <label htmlFor="category text-caption">카테고리</label>
             <select name="category" id="category" required>
               <option value="">선택해 주세요</option>
@@ -18,24 +20,33 @@ function AddRestaurantModal() {
             </select>
           </div>
 
-          <div className="form-item form-item--required">
+          <div className={styles["form-item form-item--required"]}>
             <label htmlFor="name text-caption">이름</label>
             <input type="text" name="name" id="name" required />
           </div>
 
-          <div className="form-item">
+          <div className={styles["form-item"]}>
             <label htmlFor="description text-caption">설명</label>
-            <textarea name="description" id="description" cols="30" rows="5"></textarea>
-            <span className="help-text text-caption">메뉴 등 추가 정보를 입력해 주세요.</span>
+            <textarea
+              name="description"
+              id="description"
+              cols="30"
+              rows="5"
+            ></textarea>
+            <span className={styles["help-text text-caption"]}>
+              메뉴 등 추가 정보를 입력해 주세요.
+            </span>
           </div>
 
-          <div className="button-container">
-            <button className="button button--primary text-caption">추가하기</button>
+          <div className={styles["button-container"]}>
+            <button className={styles["button button--primary text-caption"]}>
+              추가하기
+            </button>
           </div>
         </form>
       </div>
     </div>
-    );
+  );
 }
 
 export default AddRestaurantModal;
--- src/components/asides/RestaurantInfoModal.jsx
+++ src/components/asides/RestaurantInfoModal.jsx
@@ -1,18 +1,25 @@
+import styles from "./RestaurantModal.model.css";
+
 function RestaurantInfoModal() {
-    return (
-    <div className="modal modal--open">
-      <div className="modal-backdrop"></div>
-      <div className="modal-container">
-        <h2 className="modal-title text-title">음식점 이름</h2>
-        <div className="restaurant-info">
-          <p className="restaurant-info__description text-body">음식점 소개 문구</p>
+  return (
+    <div className={styles["modal modal--open"]}>
+      <div className={styles["modal-backdrop"]}></div>
+      <div className={styles["modal-container"]}>
+        <h2 className={styles["modal-title text-title"]}>음식점 이름</h2>
+
+        <div className={styles["restaurant-info"]}>
+          <p className={styles["restaurant-info__description text-body"]}>
+            음식점 소개 문구
+          </p>
         </div>
-        <div className="button-container">
-          <button className="button button--primary text-caption">닫기</button>
+        <div className={styles["button-container"]}>
+          <button className={styles["button button--primary text-caption"]}>
+            닫기
+          </button>
         </div>
       </div>
     </div>
-    );
+  );
 }
 
 export default RestaurantInfoModal;
