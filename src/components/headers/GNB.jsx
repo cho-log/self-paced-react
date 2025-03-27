4a8fd33951781d93008d986e824f44cdb79ba979
Shin Jihoon <tobeabestns@naver.com>
2025-03-27 21:23:38 +0900

    feat: GNB 모듈 생성

--- src/components/headers/GNB.jsx
+++ src/components/headers/GNB.jsx
@@ -1,8 +1,10 @@
+import styles from "./Gnb.css";
+
 function Gnb() {
   return (
-    <header className="gnb">
-      <h1 className="gnb__title text-title">점심 뭐 먹지</h1>
-      <button type="button" class="gnb__button" aria-label="음식점 추가">
+    <header className={styles.gnb}>
+      <h1 className={styles.gnb_title}>점심 뭐 먹지</h1>
+      <button type="button" className={gnb__button} aria-label="음식점 추가">
         <img src="./add-button.png" alt="음식점 추가" />
       </button>
     </header>
