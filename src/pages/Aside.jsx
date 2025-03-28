ae6d35690b44c41b8ea78b2cf9a79168eb50bdac
ChangwooJ <wjdckddn99@naver.com>
2025-03-28 21:35:50 +0900

    Feat: step2 미션을 위한 step1 카피 파일

---
+++ src/pages/Aside.jsx
@@ -0,0 +1,13 @@
+import AddRestaurantModal from "../component/aside/AddRestaurantModal";
+import RestaurantInfoModal from "../component/aside/RestaurantInfoModal";
+
+const Aside = () => {
+    return (
+        <>
+            <RestaurantInfoModal></RestaurantInfoModal>
+            <AddRestaurantModal></AddRestaurantModal>
+        </>
+    )
+}
+
+export default Aside;
