5fc8da22ae583dd1762384080c76b443ececf129
zldn109 <ss771009@naver.com>
2025-03-28 20:25:37 +0900

    fix: 헤더에 글씨 크기 조정

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import styles from "./Header.module.css";
 function Header() {
   return (
     <header className={styles.gnb}>
-      <h1 className={styles.gnb__title}>점심 뭐 먹지</h1>
+      <h1 className={`${styles.gnb__title} text-title`}>점심 뭐 먹지</h1>
       <button
         type="button"
         className={styles.gnb__button}
