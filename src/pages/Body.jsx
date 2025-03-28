7524747a2b67e6ab9ee27c9a1d831d80f7f24ea7
ChangwooJ <wjdckddn99@naver.com>
2025-03-28 22:09:08 +0900

    Feat: RestaurantList컴포넌트에서 동적으로 랜더링 할 수 있도록 수정

--- src/pages/Body.jsx
+++ src/pages/Body.jsx
@@ -1,11 +1,23 @@
+import { useState } from "react";
 import CategorySortFilter from "../component/body/CategorySortFilter";
 import RestaurantList from "../component/body/RestaurantList";
+import restaurants from "../const/restaurantList";
 
 const Body = () => {
+    const [category, setCategory] = useState("전체");
+
+    const filteredRestaurants = 
+        category === "전체" 
+        ? restaurants : restaurants.filter((restaurant) => restaurant.category === category);
+
+    const handleChangeCategory = (category) => {
+        setCategory(category);
+    }
+
     return (
         <>
-            <CategorySortFilter></CategorySortFilter>
-            <RestaurantList></RestaurantList>
+            <CategorySortFilter category={category} onChangeCategory={handleChangeCategory} />
+            <RestaurantList restaurants={filteredRestaurants} />
         </>
     )
 }
