4a74b4c8856adf6d49a1a77a34a8d210fc21a5e6
ChangwooJ <wjdckddn99@naver.com>
2025-04-29 17:45:37 +0900

    Feat: AddRestaurantModal 모달 형식 적용

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,18 @@ import "./App.css";
 import Body from "./pages/Body";
 import Header from "./pages/Header";
 import RestaurantInfoModal from "./component/aside/RestaurantInfoModal";
+import AddRestaurantModal from "./component/aside/AddRestaurantModal";
 
 function App() {
   const [isModalOpen, setIsModalOpen] = useState(null);
+  const [isAddModalOpen, setIsAddModalOpen] = useState(false);
 
   return(
     <>
-      <Header />
+      <Header setIsAddModalOpen={setIsAddModalOpen} />
       <Body setIsModalOpen={setIsModalOpen} />
       {isModalOpen && <RestaurantInfoModal setIsModalOpen={setIsModalOpen} />}
+      {isAddModalOpen && <AddRestaurantModal setIsAddModalOpen={setIsAddModalOpen} />}
     </>
   )
 }
--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,6 +1,6 @@
 import styles from '../css/Header.module.css';
 
-const Header = () => {
+const Header = ({ setIsAddModalOpen }) => {
   return (
     <header className={styles.gnb}>
       <h1 className={`text-title ${styles.gnb__title}`}>점심 뭐 먹지</h1>
@@ -8,6 +8,7 @@ const Header = () => {
         type="button"
         className={styles.gnb__button}
         aria-label="음식점 추가"
+        onClick={() => setIsAddModalOpen(true)}
       >
         <img
           className={styles.gnb__button__img}
