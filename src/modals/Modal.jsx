6c6c664723742d4318219192a0abaabec8770d80
Shin Jihoon <tobeabestns@naver.com>
2025-04-30 17:36:03 +0900

    [step_3]컨벤션 통일

--- src/modals/Modal.jsx
+++ src/modals/Modal.jsx
@@ -4,7 +4,10 @@ const Modal = ({ isOpen, onClose, children }) => {
   return (
     <div className={`${styles.modal} ${isOpen ? styles["modal--open"] : ""}`}>
       <div className={styles["modal-backdrop"]} onClick={onClose}></div>
-      <div className={styles["modal-container"]} onClick={(e) => e.stopPropagation()}>
+      <div
+        className={styles["modal-container"]}
+        onClick={(e) => e.stopPropagation()}
+      >
         {children}
       </div>
     </div>
