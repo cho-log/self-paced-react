4f4f54a8201c326d62b09415d8e632c05c57cf5c
Shin Jihoon <tobeabestns@naver.com>
2025-05-06 13:59:24 +0900

    refactor: RestaurantList를 Card 형식으로 리팩토링

--- src/components/mains/RestaurantList.jsx
+++ src/components/mains/RestaurantList.jsx
@@ -1,31 +1,17 @@
 import styles from "./RestaurantList.module.css";
+import RestaurantCard from "./RestaurantCard";
 
 function RestaurantList({ restaurants, categoryIcons, onRestaurantClick }) {
   return (
     <section className={styles["restaurant-list-container"]}>
       <ul className={styles["restaurant-list"]}>
         {restaurants.map((restaurant) => (
-          <li
+          <RestaurantCard
             key={restaurant.id}
-            className={styles["restaurant"]}
-            onClick={() => onRestaurantClick(restaurant)}
-          >
-            <div className={styles["restaurant__category"]}>
-              <img
-                src={categoryIcons[restaurant.category]}
-                alt={restaurant.category}
-                className={styles["category-icon"]}
-              />
-            </div>
-            <div className={styles["restaurant__info"]}>
-              <h3 className={`${styles["restaurant__name"]} text-subtitle`}>
-                {restaurant.name}
-              </h3>
-              <p className={`${styles["restaurant__description"]} text-body`}>
-                {restaurant.description}
-              </p>
-            </div>
-          </li>
+            restaurant={restaurant}
+            categoryIcons={categoryIcons}
+            onClick={onRestaurantClick}
+          />
         ))}
       </ul>
     </section>
