d68a60c3f9903f37eb4b92e0d92a969885509f86
zloow1009 <zloow1009@gamil.com>
2025-05-08 23:26:36 +0900

    refactor: API로 레스토랑 목록 받아오기

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Header from "./components/Header/Header.jsx";
 import MainContent from "./components/MainContent/MainContent.jsx";
 import Sidebar from "./components/Sidebar/Sidebar.jsx";
 import baseRestaurants from "./data/data.js";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [modalTypeToOpen, setModalTypeToOpen] = useState(null);
@@ -25,13 +25,24 @@ function App() {
 
   const handleCloseModal = () => setModalTypeToOpen(null);
 
+  const [restaurants, setRestaurants] = useState([]);
+  useEffect(() => {
+    const fetchRestaurants = async () => {
+      const response = await fetch("http://localhost:3000/restaurants");
+      const data = await response.json();
+      setRestaurants(data);
+    };
+
+    fetchRestaurants();
+  }, []);
+
   return (
     <>
       <Header openAddRestaurantModal={() => setModalTypeToOpen("add")} />
       <main>
         <MainContent
           onClickedDetailModal={handleClickedRestaurantInfo}
-          restaurants={updatedRestaurants}
+          restaurants={restaurants}
         />
       </main>
       <aside>
