39b6b5a586c1e597a4fea5a46f0e40217a3c2ffe
zldn109 <zloow1009@gamil.com>
2025-05-12 23:31:36 +0900

    refactor: SRP 고려하여 코드 수정

--- src/App.jsx
+++ src/App.jsx
@@ -19,25 +19,51 @@ function App() {
   };
 
   const [restaurants, setRestaurants] = useState([]);
-  useEffect(() => {
-    const fetchRestaurants = async () => {
-      const response = await fetch("http://localhost:3000/restaurants");
-      const data = await response.json();
-      setRestaurants(data);
-    };
 
-    fetchRestaurants();
-  }, []);
+  const fetchRestaurants = async () => {
+    const response = await fetch("http://localhost:3000/restaurants");
+    const data = await response.json();
+    setRestaurants(data);
+  };
 
-  const handleUpdatedRestaurants = async (restaurant) => {
+  const addNewRestaurant = async (restaurant) => {
     const response = await fetch("http://localhost:3000/restaurants", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(restaurant),
     });
     const newRestaurant = await response.json();
+    return newRestaurant;
+  };
+
+  useEffect(() => {
+    fetchRestaurants();
+  }, []);
+
+  const handleUpdatedRestaurants = async (restaurant) => {
+    const newRestaurant = await addNewRestaurant(restaurant);
     setRestaurants((prev) => [...prev, newRestaurant]);
   };
+
+  // useEffect(() => {
+  //   const fetchRestaurants = async () => {
+  //     const response = await fetch("http://localhost:3000/restaurants");
+  //     const data = await response.json();
+  //     setRestaurants(data);
+  //   };
+
+  //   fetchRestaurants();
+  // }, []);
+
+  // const handleUpdatedRestaurants = async (restaurant) => {
+  //   const response = await fetch("http://localhost:3000/restaurants", {
+  //     method: "POST",
+  //     headers: { "Content-Type": "application/json" },
+  //     body: JSON.stringify(restaurant),
+  //   });
+  //   const newRestaurant = await response.json();
+  //   setRestaurants((prev) => [...prev, newRestaurant]);
+  // };
 
   return (
     <>
