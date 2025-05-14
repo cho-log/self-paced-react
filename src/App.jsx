94daed12ccf0f6defed6ddbe9b652d01a748d150
Shin_Jihoon <140892141+developowl@users.noreply.github.com>
2025-05-14 19:54:32 +0900

    고생하셨습니다.
    :tada: PR 머지 완료! :tada:

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import { useState } from "react";
+const RESTAURANT_URL = "http://localhost:3000/restaurants";
+
+import { useState, useEffect } from "react";
 import "./App.css";
 import Gnb from "./components/headers/Gnb.jsx";
 import RestaurantCategoryFilter from "./components/mains/RestaurantCategoryFilter.jsx";
 import RestaurantList from "./components/mains/RestaurantList.jsx";
 import RestaurantInfoModal from "./components/asides/RestaurantInfoModal.jsx";
 import AddRestaurantModal from "./components/asides/AddRestaurantModal.jsx";
-import initialRestaurants from "./data/restaurants.js";
 import categoryIcons from "./data/categoryIcons.js";
 import categoryOptions from "./data/categoryOptions.js";
 import ModalTypes from "./constants/modalTypes.js";
@@ -14,7 +15,40 @@ function App() {
   const [openModal, setOpenModal] = useState(null);
   const [selectedCategory, setSelectedCategory] = useState("전체");
   const [selectedRestaurant, setSelectedRestaurant] = useState(null);
-  const [restaurants, setRestaurants] = useState(initialRestaurants);
+  const [restaurants, setRestaurants] = useState([]);
+
+  useEffect(() => {
+    fetch(RESTAURANT_URL)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setRestaurants(data);
+      })
+      .catch((error) => {
+        console.error("데이터를 불러오는 중 에러가 발생했습니다.: ", error);
+      });
+  }, []);
+
+  const handleAddRestaurant = async (restaurant) => {
+    try {
+      const response = await fetch(RESTAURANT_URL, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(restaurant),
+      });
+      if (!response.ok) throw new Error(`HTTP ${response.status}`);
+
+      const created = await response.json();
+      setRestaurants((prev) => [...prev, created]);
+      setOpenModal(null);
+    } catch (err) {
+      console.error("POST 에러:", err);
+    }
+  };
 
   const filteredRestaurants =
     selectedCategory === "전체"
@@ -22,14 +56,6 @@ function App() {
       : restaurants.filter(
           (restaurant) => restaurant.category === selectedCategory
         );
-
-  const handleAddRestaurant = (newRestaurant) => {
-    const restaurantWithId = {
-      id: Date.now(),
-      ...newRestaurant,
-    };
-    setRestaurants((prev) => [...prev, restaurantWithId]);
-  };
 
   return (
     <>
