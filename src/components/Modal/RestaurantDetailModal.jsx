14c06e861478daf9501924ee833a5895707dd84b
Johncakes <johnnygamesover9000@gmail.com>
2025-09-24 23:52:25 +0900

    style: prettier 적용

--- src/components/Modal/RestaurantDetailModal.jsx
+++ src/components/Modal/RestaurantDetailModal.jsx
@@ -1,18 +1,24 @@
-import style from './Modal.module.css';
+import style from "./Modal.module.css";
 
 export default function RestaurantDetailModal() {
-    return (
-        <div className={`${style.modal} ${style['modal--open']}`}>
-            <div className={style['modal-backdrop']}></div>
-            <div className={style['modal-container']}>
-                <h2 className={`${style['modal-title']} text-title`}>음식점 이름</h2>
-                <div className={style['restaurant-info']}>
-                    <p className={`${style['restaurant-info__description']} text-body`}>음식점 소개 문구</p>
-                </div>
-                <div className={style['button-container']}>
-                    <button className={`${style.button} ${style['button--primary']} text-caption`}>닫기</button>
-                </div>
-            </div>
+  return (
+    <div className={`${style.modal} ${style["modal--open"]}`}>
+      <div className={style["modal-backdrop"]}></div>
+      <div className={style["modal-container"]}>
+        <h2 className={`${style["modal-title"]} text-title`}>음식점 이름</h2>
+        <div className={style["restaurant-info"]}>
+          <p className={`${style["restaurant-info__description"]} text-body`}>
+            음식점 소개 문구
+          </p>
         </div>
-    )
+        <div className={style["button-container"]}>
+          <button
+            className={`${style.button} ${style["button--primary"]} text-caption`}
+          >
+            닫기
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
