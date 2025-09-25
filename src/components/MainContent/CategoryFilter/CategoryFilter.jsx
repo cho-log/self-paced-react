7fc93466b4edd33795197ed24d57eef18dd23724
dkr-sjr <rkdrjs4637@naver.com>
2025-09-25 23:34:36 +0900

    feat : 카테고리 필터 기능 추가
    카테고리 필터 기능을 추가함.
    카테고리 상태를 저장할 state 선언.
    레스트토랑 데이터 파일 분리

--- src/components/MainContent/CategoryFilter/CategoryFilter.jsx
+++ src/components/MainContent/CategoryFilter/CategoryFilter.jsx
@@ -1,9 +1,9 @@
 import styles from "./CategoryFilter.module.css"
 
-export default function CategoryFilter() {
+export default function CategoryFilter({category, onChangeCategory}) {
   return (
     <section className={styles.restaurantFilterContainer}>
-      <select name="category" id="category-filter" className={styles.restaurantFilter} aria-label="음식점 카테고리 필터">
+      <select name="category" id="category-filter" value = {category} onChange = {(e) => onChangeCategory(e.target.value)} className={styles.restaurantFilter} aria-label="음식점 카테고리 필터">
         <option value="전체">전체</option>
         <option value="한식">한식</option>
         <option value="중식">중식</option>
@@ -11,6 +11,7 @@ export default function CategoryFilter() {
         <option value="양식">양식</option>
         <option value="아시안">아시안</option>
         <option value="기타">기타</option>
+        
       </select>
     </section>
   );
