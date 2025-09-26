2b5677eb8247d50b007073c00859c4ab4befd072
Johncakes <johnnygamesover9000@gmail.com>
2025-09-26 16:06:59 +0900

    feat: useState 사용과 컴포넌트 props 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,29 @@
+import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
 import RestaurantCategoryFilter from "./components/Restaurant/RestaurantCategoryFilter/RestaurantCategoryFilter";
 import RestaurantList from "./components/Restaurant/RestaurantList/RestaurantList";
 import RestaurantDetailModal from "./components/Modal/RestaurantDetailModal";
 import AddRestaurantModal from "./components/Modal/AddRestaurantModal";
-import restaurants from "./data/restaurant";
+import getFilteredRestaurant from "./util/getFilteredRestaurant";
 
 function App() {
+  const [category, setCategory] = useState("전체");
+  const filteredRestaurants = getFilteredRestaurant(category);
+
   return (
     <>
       <Header />
       <main>
-        <RestaurantCategoryFilter />
-        <RestaurantList />
+        <RestaurantCategoryFilter
+          category={category}
+          onChangeCategory={setCategory}
+        />
+        <RestaurantList filteredRestaurants={filteredRestaurants} />
       </main>
       <aside>
-        <RestaurantDetailModal />
-        <AddRestaurantModal />
+        {/* <RestaurantDetailModal />
+        <AddRestaurantModal /> */}
       </aside>
     </>
   );
