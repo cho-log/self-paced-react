ce6edc7312605f31315c0472a79a7c01fd363c11
Johncakes <johnnygamesover9000@gmail.com>
2025-09-26 22:39:38 +0900

    refactor: return 값 읽기 쉽게 수정

--- src/util/getFilteredRestaurant.js
+++ src/util/getFilteredRestaurant.js
@@ -2,7 +2,6 @@ import restaurants from "../data/restaurant";
 
 export default function getFilteredRestaurant(category) {
   return restaurants.filter((restaurant) => {
-    if (category === "전체") return true;
-    return restaurant.category === category;
+    return category === "전체" || restaurant.category === category;
   });
 }
