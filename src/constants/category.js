76189857bcfe966653d2e1b682079b4a90491590
Johncakes <johnnygamesover9000@gmail.com>
2025-09-29 23:53:36 +0900

    refactor: Object.freeze() for categories const

--- src/constants/category.js
+++ src/constants/category.js
@@ -1,4 +1,4 @@
-const categories = [
+const categories = Object.freeze([
   { key: "all", value: "전체" },
   { key: "korean", value: "한식" },
   { key: "chinese", value: "중식" },
@@ -6,6 +6,6 @@ const categories = [
   { key: "western", value: "양식" },
   { key: "asian", value: "아시안" },
   { key: "etc", value: "기타" },
-];
+]);
 
 export default categories;
