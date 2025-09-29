34fc937b92d24c128d21d58f21978ee08a68114a
Johncakes <johnnygamesover9000@gmail.com>
2025-09-29 23:48:52 +0900

    refactor: 파일 이름 및 경로 수정

--- src/util/getFilteredRestaurant.js
+++
@@ -1,7 +0,0 @@
-import restaurants from "../data/restaurant";
-
-export default function getFilteredRestaurant(category) {
-  return restaurants.filter((restaurant) => {
-    return category === "전체" || restaurant.category === category;
-  });
-}
