5a2df07003852763d18a8aeec1b236e867aa4bc5
“윤재홍” <a287933@naver.com>
2025-10-29 23:55:10 +0900

    feat(App.jsx,HomeHeader.jsx):음식점추가모달 열기버튼과 음식점추가모달 구현

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import HomeHeader from './components/Header/HomeHeader';
 import RestaurantCategoryFilter from './components/Main/RestaurantCategoryFilter';
 import RestaurantList from './components/Main/RestaurantList';
 import RestaurantDetailModal from './components/Aside/RestaurantDetailModal';
+import AddRestaurantModal from './components/Aside/AddRestaurantModal';
 import restaurants from './data/restaurantsData';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
     : restaurants.filter((e) => e.category === selectedCategory);
 
   const [showRestaurantDetailModal, setShowRestaurantDetailModal] = useState(false);
+  const [showAddRestaurantModal, setShowAddRestaurantModal] = useState(false);
 
   const handleRestaurantClick = (restaurant) => {
     setShowRestaurantDetailModal(true);
@@ -23,7 +25,10 @@ function App() {
   return (
     <div>
 
-      <HomeHeader />
+      <HomeHeader onRestaurantAddButtonClick={() => {
+        setShowAddRestaurantModal(true);
+      }}
+      />
 
       <main>
         <RestaurantCategoryFilter
@@ -41,7 +46,11 @@ function App() {
         onClose={() => setShowRestaurantDetailModal(false)}
       />
       )}
-
+      {
+        showAddRestaurantModal && (
+          <AddRestaurantModal onClose={() => setShowAddRestaurantModal(false)} />
+        )
+      }
     </div>
   );
 }
--- src/components/Header/HomeHeader.jsx
+++ src/components/Header/HomeHeader.jsx
@@ -1,10 +1,15 @@
 import styles from './HomeHeader.module.css';
 
-function HomeHeader() {
+function HomeHeader({ onRestaurantAddButtonClick }) {
   return (
     <header className={styles.gnb}>
       <h1 className={`${styles.gnb__title} text-title`}>점심 뭐 먹지</h1>
-      <button type="button" className={styles.gnb__button} aria-label="음식점 추가">
+      <button
+        type="button"
+        onClick={onRestaurantAddButtonClick}
+        className={styles.gnb__button}
+        aria-label="음식점 추가"
+      >
         <img src="templates\add-button.png" alt="음식점 추가" />
       </button>
     </header>
