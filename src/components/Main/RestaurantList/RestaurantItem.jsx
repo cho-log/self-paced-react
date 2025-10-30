ffe1afd52470f3eb038935854a16c19a3f33a1f9
“윤재홍” <a287933@naver.com>
2025-10-31 00:12:57 +0900

    refactor(RestaurantItem.jsx, RestaurantItem.module.css):<li>태그 내에 <button>태그를 배치하여 사용자 편의성 개선

--- src/components/Main/RestaurantList/RestaurantItem.jsx
+++ src/components/Main/RestaurantList/RestaurantItem.jsx
@@ -2,22 +2,24 @@ import styles from './RestaurantItem.module.css';
 
 function RestaurantItem({ restaurant, onRestaurantClick }) {
   return (
-    <li className={styles.restaurant} onClick={() => onRestaurantClick(restaurant)}>
-      <div className={styles.restaurantCategory}>
-        <img
-          src={restaurant.image}
-          alt={restaurant.category}
-          className={styles.categoryIcon}
-        />
-      </div>
-      <div className={styles.restaurantInfo}>
-        <h3 className={`${styles.restaurantName} text-subtitle`}>
-          {restaurant.name}
-        </h3>
-        <p className={`${styles.restaurantDescription} text-body`}>
-          {restaurant.description}
-        </p>
-      </div>
+    <li className={styles.restaurant}>
+      <button type="button" className={styles.restaurantButton} onClick={() => onRestaurantClick(restaurant)}>
+        <div className={styles.restaurantCategory}>
+          <img
+            src={restaurant.image}
+            alt={restaurant.category}
+            className={styles.categoryIcon}
+          />
+        </div>
+        <div className={styles.restaurantInfo}>
+          <h3 className={`${styles.restaurantName} text-subtitle`}>
+            {restaurant.name}
+          </h3>
+          <p className={`${styles.restaurantDescription} text-body`}>
+            {restaurant.description}
+          </p>
+        </div>
+      </button>
     </li>
   );
 }
