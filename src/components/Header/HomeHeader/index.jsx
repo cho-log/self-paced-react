d316e19593e9d835258fd50138e8c0611914e25f
“윤재홍” <a287933@naver.com>
2025-11-03 14:01:38 +0900

    refactor(/HomeHeader):중복되어 있던 HomeHeader컴포넌트 단일화

--- src/components/Header/HomeHeader/index.jsx
+++ src/components/Header/HomeHeader/index.jsx
@@ -1,11 +1,17 @@
-import Button from '../../UI/Button';
 import styles from './HomeHeader.module.css';
 
-function HomeHeader() {
+function HomeHeader({ onRestaurantAddButtonClick }) {
   return (
     <header className={styles.gnb}>
       <h1 className={`${styles.gnb__title} text-title`}>점심 뭐 먹지</h1>
-      <Button />
+      <button
+        type="button"
+        onClick={onRestaurantAddButtonClick}
+        className={styles.gnb__button}
+        aria-label="음식점 추가"
+      >
+        <img src="/src/images/add-button.png" alt="음식점 추가" />
+      </button>
     </header>
   );
 }
