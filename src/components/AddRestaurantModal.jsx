a4c0ed44394a371c44b14bed5be7255b657b31e7
Yeryeong Kang <dpfud8786@gmail.com>
2025-11-04 00:16:48 +0900

    refactor: AddRestaurantModal 수정

--- src/components/AddRestaurantModal.jsx
+++ src/components/AddRestaurantModal.jsx
@@ -1,5 +1,6 @@
 import "../styles/components/Modal.css";
 import "../styles/components/Form.css";
+import Modal from "./Modal.jsx";
 
 export default function AddRestaurantModal({ onAdd, onClose }) {
   const handleSubmit = (e) => {
@@ -13,67 +14,56 @@ export default function AddRestaurantModal({ onAdd, onClose }) {
   };
 
   return (
-    <div className="modal modal--open">
-      <div className="modal-backdrop" onClick={onClose} />
-      <div
-        className="modal-container"
-        role="dialog"
-        aria-modal="true"
-        onClick={(e) => e.stopPropagation()}
-      >
-        <h2 className="modal-title text-title">새로운 음식점</h2>
+    <Modal onClose={onClose}>
+      <h2 className="modal-title text-title">새로운 음식점</h2>
 
-        <form onSubmit={handleSubmit}>
-          <div className="form-item form-item--required">
-            <label className="text-caption" htmlFor="category">
-              카테고리
-            </label>
-            <select name="category" id="category" required defaultValue="">
-              <option value="" disabled>
-                선택해 주세요
-              </option>
-              <option value="한식">한식</option>
-              <option value="중식">중식</option>
-              <option value="일식">일식</option>
-              <option value="양식">양식</option>
-              <option value="아시안">아시안</option>
-              <option value="기타">기타</option>
-            </select>
-          </div>
+      <form onSubmit={handleSubmit}>
+        <div className="form-item form-item--required">
+          <label className="text-caption" htmlFor="category">
+            카테고리
+          </label>
+          <select name="category" id="category" required defaultValue="">
+            <option value="" disabled>
+              선택해 주세요
+            </option>
+            <option value="한식">한식</option>
+            <option value="중식">중식</option>
+            <option value="일식">일식</option>
+            <option value="양식">양식</option>
+            <option value="아시안">아시안</option>
+            <option value="기타">기타</option>
+          </select>
+        </div>
 
-          <div className="form-item form-item--required">
-            <label className="text-caption" htmlFor="name">
-              이름
-            </label>
-            <input id="name" type="text" name="name" required />
-          </div>
+        <div className="form-item form-item--required">
+          <label className="text-caption" htmlFor="name">
+            이름
+          </label>
+          <input id="name" type="text" name="name" required />
+        </div>
 
-          <div className="form-item">
-            <label className="text-caption" htmlFor="description">
-              설명
-            </label>
-            <textarea
-              id="description"
-              name="description"
-              cols={30}
-              rows={5}
-              placeholder="메뉴 등 추가 정보를 입력해 주세요."
-            />
-            <span className="help-text text-caption">
-              메뉴 등 추가 정보를 입력해 주세요.
-            </span>
-          </div>
+        <div className="form-item">
+          <label className="text-caption" htmlFor="description">
+            설명
+          </label>
+          <textarea
+            id="description"
+            name="description"
+            cols={30}
+            rows={5}
+            placeholder="메뉴 등 추가 정보를 입력해 주세요."
+          />
+          <span className="help-text text-caption">
+            메뉴 등 추가 정보를 입력해 주세요.
+          </span>
+        </div>
 
-          <div className="button-container">
-            <button
-              className="button button--primary text-caption"
-              type="submit"
-            >
-              추가하기
-            </button>
-          </div>
-        </form>
-      </div>
-    </div>
+        <div className="button-container">
+          <button className="button button--primary text-caption" type="submit">
+            추가하기
+          </button>
+        </div>
+      </form>
+    </Modal>
   );
 }
