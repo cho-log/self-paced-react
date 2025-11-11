f23f9b13bc6cbba2c9b69c27b690adf5ad6d9ca1
dkr-sjr <rkdrjs4637@naver.com>
2025-11-11 22:27:40 +0900

    고생하셨습니다.
    :tada: PR 머지 완료! :tada:

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,35 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import MainContent from './components/MainContent/MainContent';
 import AsideContent from './components/AsideContent/AsideContent';
 import RestaurantDetailModal from './components/AsideContent/RestaurantDetailModal/RestaurantDetailModal';
 import AddRestaurantModal from './components/AsideContent/AddRestaurantModal/AddRestaurantModal';
-import defaultRestaurantInfoList from './Data/restaurantInfoList';
+import { getRestaurantInfoList, addNewRestaurantInfo } from './api/restaurantApi';
 import './App.css';
 
 function App() {
-  const [restaurantInfoList, setRestaurantInfoList] = useState(defaultRestaurantInfoList);
-  const addRestaurantInfo = (restaurantInfo) => {
-    setRestaurantInfoList([...restaurantInfoList, restaurantInfo]);
+  const [restaurantInfoList, setRestaurantInfoList] = useState([]);
+
+  const fetchRestaurantInfoList = async () => {
+    const response = await getRestaurantInfoList();
+    if (response.success) {
+      setRestaurantInfoList(response.data);
+    } else {
+      alert(response.error);
+    }
   };
+  const addRestaurantInfo = async (restaurantInfo) => {
+    const response = await addNewRestaurantInfo(restaurantInfo);
+
+    if (response.success) {
+      fetchRestaurantInfoList();
+    } else {
+      alert(response.error);
+    }
+  };
+
+  useEffect(() => {
+    fetchRestaurantInfoList();
+  }, []);
 
   const [isVisibleAddRestaurantModal, setIsVisibleAddRestaurantModal] = useState(false);
   const showAddRestaurantModal = () => {
--- src/components/AsideContent/AddRestaurantModal/AddRestaurantModal.jsx
+++ src/components/AsideContent/AddRestaurantModal/AddRestaurantModal.jsx
@@ -2,14 +2,9 @@ import styles from '../RestaurantModal.module.css';
 import Modal from '../Modal/Modal';
 import categoryList from '../../../Data/categoryList';
 
-const categoryImgMap = {
-  한식: 'templates/category-korean.png',
-  중식: 'templates/category-chinese.png',
-  일식: 'templates/category-japanese.png',
-  양식: 'templates/category-western.png',
-  아시안: 'templates/category-asian.png',
-  기타: 'templates/category-etc.png',
-};
+const optionList = categoryList.filter((value) => (value !== '전체')).map((value) => (
+  <option value={value} key={value}>{value}</option>
+));
 
 export default function AddRestaurantModal({ isVisible, closeModal, addRestaurantInfo }) {
   const handleSubmit = (e) => {
@@ -21,15 +16,10 @@ export default function AddRestaurantModal({ isVisible, closeModal, addRestauran
       name: formData.get('name'),
       description: formData.get('description'),
       category: formData.get('category'),
-      imgSrc: categoryImgMap[formData.get('category')],
     };
     addRestaurantInfo(newRestaurant);
     closeModal();
   };
-
-  const optionList = categoryList.map((value) => (
-    <option value={value} key={value}>{value}</option>
-  ));
 
   if (!isVisible) return null;
   return (
