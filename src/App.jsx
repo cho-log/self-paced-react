f600dd252e6d57a99d1b24a9bd403c4615b94096
gyuminJJANG <ggavwu789@gmail.com>
2026-04-07 13:51:52 +0900

    feat: 1단계 - Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import "./App.css";
+import Header from './components/Header/Header';
+import CategoryFilter from "./components/CategoryFilter/CategoryFilter";
+import RestaurantList from "./components/RestaurantList/RestaurantList";
+import RestaurantDetailModal from "./components/RestaurantDetailModal/RestaurantDetailModal";
+import AddRestaurantModal from "./components/AddRestaurantModal/AddRestaurantModal";
 
 function App() {
-  return <h1>Self-Paced React</h1>;
+  return ( 
+    <>
+      <Header />
+      <main>
+        <CategoryFilter />
+        <RestaurantList />
+      </main>
+      <aside>
+        <RestaurantDetailModal />
+        <AddRestaurantModal />
+      </aside>
+    </>
+  );
 }
 
 export default App;
