9019c2c40ee7f8e9484668f05ca5eadf8d526fa1
Hong Eui-min <78837018+EM-H20@users.noreply.github.com>
2026-04-15 01:40:43 +0900

    고생하셨습니다.
    :tada: PR 머지 완료! :tada:

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,46 @@
 import "./App.css";
+import Header from "./components/Header";
+import CategoryFilter from "./components/CategoryFilter";
+import RestaurantList from "./components/RestaurantList";
+import RestaurantDetailModal from "./components/RestaurantDetailModal";
+import AddRestaurantModal from "./components/AddRestaurantModal";
+import { restaurants } from "./constants/restaurants";
+
+import { useState } from "react";
 
 function App() {
-  return <h1>Self-Paced React</h1>;
+  const [category, setCategory] = useState("전체");
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedRestaurant, setSelectedRestaurant] = useState(null);
+  const filteredRestaurants =
+    category === "전체"
+      ? restaurants
+      : restaurants.filter((restaurant) => restaurant.category === category);
+
+  return (
+    <>
+      <Header category={category} />
+      <main>
+        <CategoryFilter category={category} onChangeCategory={setCategory} />
+        <RestaurantList
+          restaurants={filteredRestaurants}
+          onRestaurantClick={(restaurant) => {
+            setSelectedRestaurant(restaurant);
+            setIsModalOpen(true);
+          }}
+        />
+      </main>
+      <aside>
+        {isModalOpen && (
+          <RestaurantDetailModal
+            restaurant={selectedRestaurant}
+            onClose={() => setIsModalOpen(false)}
+          />
+        )}
+        <AddRestaurantModal />
+      </aside>
+    </>
+  );
 }
 
 export default App;
