cf7a67426ece83efbe604808a959ef7c37192445
gyuminJJANG <ggavwu789@gmail.com>
2026-05-10 19:15:13 +0900

    refactor: 모달 표준 문법 수정

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -2,7 +2,7 @@ import "./Modal.css";
 
 export default function Modal({ title, children, onClose }) {
     return (
-        < >
+        <>
             <div className="modal-backdrop" onClick={onClose}></div>
             <div className="modal-container">
                 <h2 className="modal-title text-title">{title}</h2>
