0d34a3dd9ed2a5ff365b90427232ee919c1be18a
kokunut <ggavwu789@gmail.com>
2026-05-15 03:41:37 +0900

    고생하셨습니다.
    :tada: PR 머지 완료! :tada:

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,90 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
-import { INIT_RESTAURANT } from "./RestaurantData";
-import Header from './components/Header/Header';
+
+import Header from "./components/Header/Header";
 import CategoryFilter from "./components/CategoryFilter/CategoryFilter";
 import RestaurantList from "./components/RestaurantList/RestaurantList";
 import RestaurantDetailModal from "./components/RestaurantDetailModal/RestaurantDetailModal";
 import AddRestaurantModal from "./components/AddRestaurantModal/AddRestaurantModal";
 
+const BASE_URL = "http://localhost:3000/restaurants";
+
 function App() {
+  const [restaurants, setRestaurants] = useState([]);
 
-  const [category, setCategory]=useState("전체");
+  const [category, setCategory] = useState("전체");
 
-  const [isModalOpen, setIsModalOpen]=useState(false);
+  const [isDetailModalOpen, setIsDetailModalOpen] = useState(false);
+
+  const [isAddModalOpen, setIsAddModalOpen] = useState(false);
 
   const [selectedRestaurant, setSelectedRestaurant] = useState(null);
 
-  const filteredRestaurants=INIT_RESTAURANT.filter((restaurant)=>{
-    if(category==="전체"){
+  const fetchRestaurants = async () => {
+    try {
+      const response = await fetch(BASE_URL);
+      const data = await response.json();
+      setRestaurants(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchRestaurants();
+  }, []);
+
+  const filteredRestaurants = restaurants.filter((restaurant) => {
+    if (category === "전체") {
       return true;
     }
-    const SameCategory=restaurant.category===category;
+    const SameCategory = restaurant.category === category;
     return SameCategory;
-  })
+  });
+
+  const handleAddRestaurant = async (newRestaurant) => {
+    const res = await fetch(BASE_URL, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newRestaurant),
+    });
+
+    const data = await res.json();
+
+    setRestaurants((prev) => [...prev, data]);
+
+    setIsAddModalOpen(false);
+  };
 
   const handleOpenModal = (item) => {
     setSelectedRestaurant(item);
-    setIsModalOpen(true);
+    setIsDetailModalOpen(true);
   };
 
-  const handleCloseModal = () => {
-    setIsModalOpen(false);
-  };
-
-  return ( 
+  return (
     <>
-      <Header />
+      <Header setIsAddModalOpen={() => setIsAddModalOpen(true)} />
       <main>
         <CategoryFilter category={category} setCategory={setCategory} />
-        <RestaurantList restaurants={filteredRestaurants} category={category} 
-          onOpenModal={handleOpenModal} />
+        <RestaurantList
+          restaurants={filteredRestaurants}
+          category={category}
+          onOpenModal={handleOpenModal}
+        />
       </main>
       <aside>
-        {isModalOpen && <RestaurantDetailModal 
-          restaurants={selectedRestaurant}
-          onCloseModal={handleCloseModal}/>}
-        <AddRestaurantModal />
+        {isDetailModalOpen && (
+          <RestaurantDetailModal
+            restaurants={selectedRestaurant}
+            onClose={() => setIsDetailModalOpen(false)}
+          />
+        )}
+        {isAddModalOpen && (
+          <AddRestaurantModal
+            onClose={() => setIsAddModalOpen(false)}
+            onAdd={handleAddRestaurant}
+          />
+        )}
       </aside>
     </>
   );
