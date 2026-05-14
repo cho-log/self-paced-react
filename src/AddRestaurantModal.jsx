73457ff04ba3064d6a8032b347bd1528b81461c2
realcdh <rdhdevelop@gmail.com>
2026-05-14 23:20:39 +0900

    고생하셨습니다.
    :tada: PR 머지 완료! :tada:

--- src/AddRestaurantModal.jsx
+++ src/AddRestaurantModal.jsx
@@ -1,10 +1,49 @@
-export default function AddRestaurantModal() {
+import { useState } from 'react';
+
+export default function AddRestaurantModal({
+	onAddRestaurant,
+	setIsAddModalOpen,
+}) {
+	const [isSubmitting, setIsSubmitting] = useState(false);
+
+	const handleCloseModal = () => {
+		setIsAddModalOpen(false);
+	};
+
+	const handleAddRestaurant = async (e) => {
+		e.preventDefault();
+		const form = e.target;
+		const category = form.category.value;
+		const name = form.name.value;
+		const description = form.description.value;
+
+		const newRestaurant = {
+			id: `a${Date.now()}`,
+			category,
+			name,
+			description,
+		};
+
+		setIsSubmitting(true);
+
+		try {
+			const isAdded = await onAddRestaurant(newRestaurant);
+
+			if (isAdded) {
+				form.reset();
+				handleCloseModal();
+			}
+		} finally {
+			setIsSubmitting(false);
+		}
+	};
+
 	return (
 		<div className="modal modal--open">
-			<div className="modal-backdrop"></div>
+			<div className="modal-backdrop" onClick={handleCloseModal}></div>
 			<div className="modal-container">
 				<h2 className="modal-title text-title">새로운 음식점</h2>
-				<form>
+				<form onSubmit={handleAddRestaurant}>
 					<div className="form-item form-item--required">
 						<label htmlFor="category" className="text-caption">
 							카테고리
@@ -35,16 +74,18 @@ export default function AddRestaurantModal() {
 							name="description"
 							id="description"
 							cols="30"
-							rows="5"
-						></textarea>
+							rows="5"></textarea>
 						<span className="help-text text-caption">
 							메뉴 등 추가 정보를 입력해 주세요.
 						</span>
 					</div>
 
 					<div className="button-container">
-						<button className="button button--primary text-caption">
-							추가하기
+						<button
+							className="button button--primary text-caption"
+							type="submit"
+							disabled={isSubmitting}>
+							{isSubmitting ? '추가 중...' : '추가하기'}
 						</button>
 					</div>
 				</form>
